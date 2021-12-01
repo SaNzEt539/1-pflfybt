c8f5777293e4b9ca0ae86a117f38f32189233900
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-01 19:35:51 +0300

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число : ");
+            int n = int.Parse(Console.ReadLine());
+            int a = n / 100;
+            int c = n % 10;
+            int b = n % 100 / 10;
+            if (a == b && b == c)
+            {
+                Console.WriteLine("Цифры трехзначного числа одинаковые");
+            }
+            else
+            {
+                Console.WriteLine("Цифры трехзначного числа не одинаковые");
+            }
+        }
+    }
+}
